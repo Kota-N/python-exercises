133e3a8cd992953f7ff4f2bcfefd0759d3cd3e8b
Kota <kotancell@gmail.com>
2020-10-23 22:11:17 -0400

    Initial commit

---
+++ practice.py
@@ -0,0 +1,14 @@
+# 1. Character Input
+
+name = input('What is your name?: ')
+age = int(input('How old are you?: '))
+
+if age < 100:
+    print('Hi ' + name + '! You will be 100 years old in ' + str(100 - age) + ' years.')
+else:
+    print('Oh no, you have already reached 100 years old!')
+
+
+
+# 2. Odd or Even
+
