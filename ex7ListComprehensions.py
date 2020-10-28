01c0d35d921716929866e34f0df295b70571db45
Kota <kotancell@gmail.com>
2020-10-27 21:50:30 -0400

    Add ex7 and change file names

---
+++ ex7ListComprehensions.py
@@ -0,0 +1,9 @@
+# Exercises from http://www.practicepython.org/
+
+#7 List Comprehensions
+
+num_list = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+even_num = [x for x in num_list if x % 2 == 0]
+
+print(even_num)
